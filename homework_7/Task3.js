3bb83adf29c4f005d620f94ac2f93c8f73ecadd3
Aksana Shyshla <oksanash1507@gmail.com>
2025-11-09 14:09:35 +0300

    [homework_7] fixed bug in task 3

--- homework_7/Task3.js
+++ homework_7/Task3.js
@@ -2,24 +2,29 @@
 // Ваш код должен иметь возможность считать количество дней на любую заданую в переменной первоначальную дату и
 // считать верно через 10-15-20 лет (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date )
 
+
 function quantityOfFriday13(date) {
     const startDate = new Date(date);
     const today = new Date();
     let counter = 0;
+    let year = startDate.getFullYear();
+    let month = startDate.getMonth();
 
-    while (startDate <= today) {
-        const year = startDate.getFullYear();
-        const month = startDate.getMonth();
+    while (year < today.getFullYear() || (year === today.getFullYear() && month <= today.getMonth())) {
 
         const dateOf13 = new Date(year, month, 13);
         if (dateOf13 >= startDate && dateOf13 <= today && dateOf13.getDay() === 5) {
             counter++;
-
         }
-        startDate.setMonth(startDate.getMonth() + 1);
+        month++;
+        if (month > 11) {
+            month = 0;
+            year++;
+        }
     }
+
     return counter;
 }
 
-const date = new Date("November 20, 2020 00:20:18");
-console.log(quantityOfFriday13(date));
+const testDate = new Date("November 20, 2024 00:20:18");
+console.log(quantityOfFriday13(testDate));
